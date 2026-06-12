fa9b9a40ddad597a80656c267623c5e3e8e5b0c2
Neramit <DavidTh30@gmail.com>
2026-06-12 19:41:56 +0700

    Add files via upload

---
+++ obj3d_04/unit1.pas
@@ -0,0 +1,132 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, ComCtrls,
+  BGRAOpenGL, BGRAVirtualScreen, BGRABitmap, BGRAScene3D;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    BGRAVirtualScreen1: TBGRAVirtualScreen;
+    StatusBar1: TStatusBar;
+    Timer1: TTimer;
+    procedure BGRAVirtualScreen1Redraw(Sender: TObject; Bitmap: TBGRABitmap);
+    procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
+    procedure FormShow(Sender: TObject);
+    procedure Timer1Timer(Sender: TObject);
+  private
+    scene: TBGRAScene3D;
+    obj3D: IBGRAObject3D;
+    procedure DrawPyramid(rotX, rotY, rotZ: integer);
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+uses
+  BGRABitmapTypes;
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.FormShow(Sender: TObject);
+begin
+  self.Caption := 'TBGRAVirtualScreen test';
+  scene := TBGRAScene3D.Create;
+  Timer1.Enabled := True;
+end;
+
+procedure TForm1.FormClose(Sender: TObject; var CloseAction: TCloseAction);
+begin
+  Timer1.Enabled := False;
+  if Assigned(scene) then
+    FreeAndNil(scene);
+  if Assigned(obj3D) then
+    FreeAndNil(obj3D);
+end;
+
+procedure TForm1.BGRAVirtualScreen1Redraw(Sender: TObject; Bitmap: TBGRABitmap);
+begin
+  Bitmap.FillRect(rect(0, 0, Bitmap.Width, Bitmap.Height), BGRABlack, dmSet);
+  scene.Surface := Bitmap;
+  scene.Render;
+end;
+
+procedure TForm1.Timer1Timer(Sender: TObject);
+const
+  x: integer = 0;
+  y: integer = 0;
+  z: integer = 0;
+  start: Qword = 0;
+begin
+  Timer1.Enabled := False;
+
+  // Update display with previous values
+  StatusBar1.Panels[3].Text := IntToStr(x) + 'º';
+  StatusBar1.Panels[5].Text := IntToStr(y) + 'º';
+  StatusBar1.Panels[7].Text := IntToStr(z) + 'º';
+
+  StatusBar1.Panels[1].Text := IntToStr(GetTickCount64 - start) + ' ms';
+  start := GetTickCount64;
+  DrawPyramid(x, y, z);
+  BGRAVirtualScreen1.RedrawBitmap;
+  Application.ProcessMessages;
+
+  // Simulate some motion
+  Inc(x, 1);
+  if x > 360 then
+    x := x - 360;
+
+  Inc(y, 3);
+  if y > 360 then
+    y := y - 360;
+
+  Inc(z, 4);
+  if z > 360 then
+    z := z - 360;
+
+  Timer1.Enabled := True;
+end;
+
+procedure TForm1.DrawPyramid(rotX, rotY, rotZ: integer);
+var
+  base: array of IBGRAVertex3D;
+  topV: IBGRAVertex3D;
+begin
+  scene.Clear;
+
+  //create a pyramid
+  obj3D := scene.CreateObject(BGRA(255, 240, 128));
+  with obj3D do
+  begin
+    //create vertices
+    topV := MainPart.Add(0, -50, 0);
+    //pyramid base is in a clockwise order if we look at the pyramid from below
+    base := MainPart.Add([-20, 25, -20, 20, 25, -20, 20, 25, 20, -20, 25, 20]);
+
+    AddFace(base, BGRA(127, 127, 127));
+    //add four faces, the three vertices are in a clockwise order
+    AddFace([base[0], topV, base[1]], BGRA(255, 0, 0));
+    AddFace([base[1], topV, base[2]], BGRA(0, 255, 0));
+    AddFace([base[2], topV, base[3]], BGRA(0, 0, 255));
+    AddFace([base[3], topV, base[0]]);
+    topV := nil;
+    base := nil;
+  end;
+  obj3D.MainPart.RotateYDeg(rotY);
+  obj3D.MainPart.RotateXDeg(rotX);
+  obj3D.MainPart.RotateZDeg(rotZ);
+end;
+
+end.
